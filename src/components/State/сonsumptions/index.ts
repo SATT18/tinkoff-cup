ca4f947238886dd93b2dd477c2301c85f3236ed1
Andrey Semenov <semnov.andrey@gmail.com>
2023-04-22 16:53:51 +0300

    feat: add sorts

--- src/components/State/сonsumptions/index.ts
+++ src/components/State/сonsumptions/index.ts
@@ -1,6 +1,8 @@
-import { atom } from 'recoil'
+import { atom, selector } from 'recoil'
 import { recoilPersist } from 'recoil-persist'
 import { Category } from '../categories'
+import dayjs from 'dayjs'
+
 const { persistAtom } = recoilPersist()
 
 type Consumption = {
@@ -15,4 +17,56 @@ export const ConsumptionsState = atom<Consumption[]>({
   key: 'consumptions',
   default: [],
   effects_UNSTABLE: [persistAtom],
+})
+
+export type ConsumptionsFilter = {
+  category?: string
+  sortField: string
+  sort: string
+  dateFrom?: string
+  dateTo?: string
+}
+export const ConsumptionsFilterState = atom<ConsumptionsFilter>({
+  key: 'consumptionsFilter',
+  default: {
+    category: '',
+    sortField: 'date',
+    sort: 'DESC',
+  },
+  effects_UNSTABLE: [persistAtom],
+})
+
+export const ConsumptionsStateFiltered = selector({
+  key: 'consumptionsFiltered',
+  get: ({ get }) => {
+    const consumptions = get(ConsumptionsState)
+    const filters = get(ConsumptionsFilterState)
+
+    let output = [...consumptions]
+
+    // category filter
+    if (filters.category) {
+      output = output.filter((el) => el.category.id === filters.category)
+    }
+
+    // date filter
+    if (filters.dateFrom) {
+      output = output.filter((el) => dayjs(el.date) >= dayjs(filters.dateFrom))
+    }
+    if (filters.dateTo) {
+      output = output.filter((el) => dayjs(el.date) <= dayjs(filters.dateTo))
+    }
+
+    // sorts
+    if (filters.sort && filters.sortField) {
+      if (filters.sortField === 'date' && filters.sort === 'ASC') {
+        output.sort((a, b) => (dayjs(a.date) > dayjs(b.date) ? 1 : -1))
+      }
+      if (filters.sortField === 'date' && filters.sort === 'DESC') {
+        output.sort((a, b) => (dayjs(a.date) < dayjs(b.date) ? 1 : -1))
+      }
+    }
+
+    return output
+  },
 })
--- src/components/pages/Consumptions/index.tsx
+++ src/components/pages/Consumptions/index.tsx
@@ -1,29 +1,98 @@
 import { AddConsumption } from '../../organisms/AddСonsumption'
-import { ConsumptionsState } from '../../State/сonsumptions'
-import { List } from 'antd'
+import { Button, DatePicker, Form, List, Modal, Select } from 'antd'
+import { CategoriesOptions } from '../../State/categories'
+import {
+  ConsumptionsFilterState,
+  ConsumptionsStateFiltered,
+} from '../../State/сonsumptions'
+import { FilterOutlined } from '@ant-design/icons'
 import { formatAmount } from '../../../lib/formatAmount'
 import { formatDate } from '../../../lib/formatDateToTimeDate'
-import { useRecoilValue } from 'recoil'
+import { useRecoilState, useRecoilValue } from 'recoil'
 import MainTemplate from '../../templates/MainTemplate'
-import React, { useMemo } from 'react'
-import dayjs from 'dayjs'
+import React, { useMemo, useState } from 'react'
+import dayjs, { Dayjs } from 'dayjs'
 
+const { RangePicker } = DatePicker
+
+type FilterValues = {
+  category?: { label: string; value: string }
+  date?: [Dayjs, Dayjs]
+}
 const Consumptions = () => {
-  const consumptions = useRecoilValue(ConsumptionsState)
+  const consumptions = useRecoilValue(ConsumptionsStateFiltered)
+  const [filtersState, setFilters] = useRecoilState(ConsumptionsFilterState)
 
-  const data = useMemo(
-    () =>
-      [...consumptions].sort((a, b) =>
-        dayjs(a.date) > dayjs(b.date) ? 1 : -1
-      ),
-    [consumptions]
+  const categoriesOptions = useRecoilValue(CategoriesOptions)
+  const targetOptions = useMemo(
+    () => [{ label: 'Все', value: '' }, ...categoriesOptions],
+    [categoriesOptions]
   )
+
+  const handleFilterFinish = (values: FilterValues) => {
+    const dateFrom = values.date?.[0]
+    const dateTo = values.date?.[1]
+
+    setFilters({
+      category: values.category?.value,
+      dateFrom: dateFrom ? dateFrom.toISOString() : undefined,
+      dateTo: dateTo ? dateTo.toISOString() : undefined,
+      sort: 'ASC',
+      sortField: 'date',
+    })
+
+    setFiltersOpen(false)
+  }
+  const [isFiltersOpen, setFiltersOpen] = useState(false)
 
   return (
     <MainTemplate title="Ваши траты">
+      <Button onClick={() => setFiltersOpen(true)}>
+        <FilterOutlined /> Фильтры
+      </Button>
+      <Modal
+        destroyOnClose
+        footer={null}
+        onCancel={() => setFiltersOpen(false)}
+        open={isFiltersOpen}
+        title="Фильтры"
+      >
+        <Form<FilterValues>
+          initialValues={{
+            category: targetOptions.find(
+              (el) => el.value === filtersState.category
+            ),
+            date: [
+              filtersState.dateFrom ? dayjs(filtersState.dateFrom) : undefined,
+              filtersState.dateTo ? dayjs(filtersState.dateTo) : undefined,
+            ],
+          }}
+          labelCol={{ span: 8 }}
+          name="filter"
+          onFinish={handleFilterFinish}
+          wrapperCol={{ span: 14 }}
+        >
+          <Form.Item
+            label="Категория"
+            name="category"
+            style={{
+              minWidth: 250,
+            }}
+          >
+            <Select labelInValue options={targetOptions} />
+          </Form.Item>
+          <Form.Item label="Период" name="date">
+            <RangePicker />
+          </Form.Item>
+          <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+            <Button htmlType="submit" type="primary">
+              Фильтровать
+            </Button>
+          </Form.Item>
+        </Form>
+      </Modal>
       <List
-        dataSource={data}
-        itemLayout="horizontal"
+        dataSource={consumptions}
         renderItem={(item, index) => (
           <List.Item key={item.id}>
             <List.Item.Meta
